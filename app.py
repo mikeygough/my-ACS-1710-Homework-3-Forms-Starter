ba8348e6087975a70a8b6acd93a174cac51dcd34
mikey <mikey@hey.com>
2023-11-05 16:05:06 -0600

    add fact categories

--- app.py
+++ app.py
@@ -78,12 +78,24 @@ def compliments_results():
 ################################################################################
 
 animal_to_fact = {
-    'koala': 'Koala fingerprints are so close to humans\' that they could taint crime scenes.',
-    'parrot': 'Parrots will selflessly help each other out.',
-    'mantis shrimp': 'The mantis shrimp has the world\'s fastest punch.',
-    'lion': 'Female lions do 90 percent of the hunting.',
-    'narwhal': 'Narwhal tusks are really an "inside out" tooth.',
-    'whale shark': 'Whale sharks are the largest shark and the largest of any fishes alive today.'
+    'koala': {'habitat': 'Koalas are native to Australia and are primarily found in eucalyptus forests and woodlands. They spend most of their time in the trees.',
+              'physical_characteristics': 'Koalas are marsupials and have a unique diet of eucalyptus leaves. They have a specialized digestive system adapted to this diet.',
+              'intelligence': 'Koalas have a relatively simple brain structure and are not known for their high intelligence. They rely on instinctual behaviors for survival.'},
+    'parrot': {'habitat': 'Parrots are found in a wide range of habitats, including tropical rainforests, deserts, and coastal regions. They are known for their adaptability.',
+              'physical_characteristics': 'Parrots are known for their colorful plumage and strong, curved beaks, which they use for cracking nuts and seeds.',
+              'intelligence': 'Parrots are highly intelligent birds. They are known for their ability to mimic human speech and solve complex puzzles.'},
+    'mantis shrimp': {'habitat': 'Mantis shrimp are marine crustaceans found in shallow tropical and subtropical waters. They are often found in burrows in the seabed.',
+              'physical_characteristics': 'Mantis shrimp have highly developed appendages with powerful claws that they use to strike their prey with incredible speed and force.',
+              'intelligence': 'Mantis shrimp are not known for their intelligence in the same way that mammals are, but they have remarkable visual capabilities and hunting strategies.',},
+    'lion': {'habitat': 'Lions are native to Africa and can be found in various habitats, including savannas, grasslands, and open woodlands. They are social animals that live in prides.',
+              'physical_characteristics': 'Lions are known for their muscular bodies, manes (in males), and sharp teeth. They are apex predators in their ecosystems.',
+              'intelligence': 'Lions are highly social animals with complex behaviors. They have a well-developed hunting strategy and work together in coordinated group hunts.',},
+    'narwhal': {'habitat': 'Narwhals inhabit the Arctic waters of the North Atlantic and the North Pacific. They are often associated with sea ice and cold, deep oceans.',
+              'physical_characteristics': 'Narwhals are known for their long spiral tusks, which are actually elongated teeth. These tusks can grow up to 10 feet in length.',
+              'intelligence': 'Narwhals are not extensively studied, but they exhibit complex behaviors and are adapted to their challenging Arctic environment.'},
+    'whale shark': {'habitat': 'Whale sharks are found in warm, tropical waters around the world. They are known for their seasonal migrations and are often spotted near coral reefs.',
+              'physical_characteristics': 'Whale sharks are the largest fish in the ocean, with a distinctive pattern of white spots on their dark blue-gray skin.',
+              'intelligence': 'Whale sharks are not considered highly intelligent; they are filter feeders, primarily driven by instinct and their need for food.'}
 }
 
 @app.route('/animal_facts')
@@ -91,17 +103,16 @@ def animal_facts():
     """Show a form to choose an animal and receive facts."""
 
     # get form values:
-    # animals_to_display = request.args.get('animal')
     animals_to_display = request.args.getlist('animal')
+    fact_category = request.args.get('fact_category')
     
     # populate context
     context = {
-        # TODO: Enter your context variables here for:
         'animal_to_fact': animal_to_fact,
         'animal_list': animal_to_fact.keys(),
-        'animals_to_display': animals_to_display
+        'animals_to_display': animals_to_display,
+        'fact_category': fact_category
     }
-    print(animals_to_display)
 
     # print(animal)
     return render_template('animal_facts.html', **context)
